32c343c4c7d0c85768d8d145f4a0d9161ac837a5
NikolayNesterchuk <nn@handiscover.com>
2024-03-15 10:16:20 +0100

    Green button fullscreen

--- src/components/modal/components/sidebar/components/window-buttons/WindowButtons.tsx
+++ src/components/modal/components/sidebar/components/window-buttons/WindowButtons.tsx
@@ -13,8 +13,8 @@ const WindowButtons = () => {
   return (
     <div className={styles.buttons}>
       <div className={styles.circle} style={{background: '#ff6057'}}></div>
-      <div className={styles.circle} onClick={toggleFullscreenMode} style={{background: '#fabe31'}}></div>
-      <div className={styles.circle} style={{background: '#2ac73f'}}></div>
+      <div className={styles.circle} style={{background: '#fabe31'}}></div>
+      <div className={styles.circle} onClick={toggleFullscreenMode} style={{background: '#2ac73f'}}></div>
     </div>
   )
 }
