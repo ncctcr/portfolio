9c2873db90b11bd230b525ef84229425177484e9
NikolayNesterchuk <nn@handiscover.com>
2024-03-15 11:06:18 +0100

    Added hook theme detector + adapt theme by browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Modal from "./components/modal/Modal";
 import Background from "./components/background/Background";
-import { useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {changeThemeAction} from "./redux/actions/settingsActions";
+import useThemeDetector from "./hooks/useThemeDetector";
 
 const App = () => {
+  const dispatch = useDispatch();
   const theme = useSelector((state: any) => state.settings.theme);
   const blobColor = useSelector((state: any) => state.settings.blob.color);
   const blur = useSelector((state: any) => state.settings.blob.blur);
+  const isDarkTheme = useThemeDetector();
+
+  useEffect(() => {
+    dispatch(changeThemeAction(isDarkTheme ? 'dark' : 'light'));
+  }, []);
 
   return (
     <div className="App" data-theme={theme}>
---
+++ src/hooks/useThemeDetector.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useThemeDetector = (): boolean => {
+  const getCurrentTheme = (): boolean => window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(getCurrentTheme());
+  const mqListener = (e: MediaQueryListEvent): void => {
+    setIsDarkTheme(e.matches);
+  };
+
+  useEffect(() => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    darkThemeMq.addListener(mqListener);
+    return () => darkThemeMq.removeListener(mqListener);
+  }, []);
+
+  return isDarkTheme;
+}
+
+export default useThemeDetector;
